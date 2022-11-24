5a49d71b0c14b0bf30842ee625533374ff422813
Lichfolky <lichfolky@gmail.com>
2022-11-24 13:04:07 +0100

    fix

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 
 ## [Argomenti lezione 1](1-intro-variabili-condizioni.md) del 14 Nov 2022  
 - Esercizio: [Un flowchart su come fare la pasta](https://www.figma.com/file/k0M7EjNgv7DJxqu54JVWgg/Untitled?node-id=24%3A28)
-- Esercizio: [Un test infallibile che ti dice se sei un tipo da mare o da montagna](/test-mare-montagna)
+- Esercizio: [Un test infallibile che ti dice se sei un tipo da mare o da montagna](https://github.com/lichfolky/elementi-js-2022/tree/main/test-mare-montagna/)
   
 ## [Argomenti lezione 2](2-logica-cicli.md) del 17 Nov 2022  
-- Esercizio: [Calcolatore del BMI](/bmi-calc)
+- Esercizio: [Calcolatore del BMI](https://github.com/lichfolky/elementi-js-2022/tree/main/bmi-calc/)
 
 ## [Argomenti lezione 3](3-arrays.md) del 24 Nov 2022   
-- Esercizio: [Esperimenti con il DOM e le tabelline](/lista-spesa-tabelline)  
-- Esercizi da fare a casa: [esercizi-base-array](/esercizi-base-array)
+- Esercizio: [Esperimenti con il DOM e le tabelline](https://github.com/lichfolky/elementi-js-2022/tree/main/lista-spesa-tabelline/)  
+- Esercizi da fare a casa: [esercizi-base-array](https://github.com/lichfolky/elementi-js-2022/tree/main/esercizi-base-array/)
+- Esercizio: [cornicetta](https://github.com/lichfolky/elementi-js-2022/tree/main/cornicetta/)
---
+++ cornicetta/script.js
@@ -0,0 +1,71 @@
+let str = "Pippo pippo non lo sa";
+//console.log("prima lettera: " + str[0]);
+let parole = str.split(" ");
+//console.log(parole);
+//console.log(parole.join("-"));
+/*
+let nomeFile = "esercizio-cornicetta-ok";
+parole = nomeFile.split("-");
+console.log(parole);
+*/
+/*/*
+ESERCIZIO
+Data la stringa "Pippo pippo non lo sa" creare:
+
+*********
+* Pippo *
+* pippo *
+* non   *
+* lo    *
+* sa    *
+*********
+*/
+
+// stampare la prima riga
+// per ogni parola della stringa
+//     stampare "* parola *"
+// stampare ultima riga
+
+let testo = "Pippo pippo non lo sa ayeyee";
+let maxParola = "";
+let maxParolaNum = 0;
+
+/*
+trova parola più lunga
+stampa * pari a lunghezza parola + 4
+*/
+function stampaRiga() {
+    let parole = testo.split(" ");
+
+    maxParola = "";
+    maxParolaNum = 0;
+
+    for (parola of parole) {
+        if (parola.length > maxParolaNum) {
+            maxParola = parola;
+            maxParolaNum = parola.length;
+        }
+    }
+    // console.log(maxParola, maxParolaNum);
+    let asterischi = "";
+    for (let i = 0; i < maxParolaNum + 4; i++) {
+        asterischi += "*";
+    }
+    console.log(asterischi);
+}
+function stampaParole() {
+    let parole = testo.split(" ");
+    for (parola of parole) {
+        let daStampare = "* ";
+        daStampare += parola;
+        for (let i = 0; i < maxParolaNum - parola.length; i++) {
+            daStampare += " ";
+        }
+        daStampare += " *";
+        console.log(daStampare);
+    }
+}
+
+stampaRiga();
+stampaParole();
+stampaRiga();
